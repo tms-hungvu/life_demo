3764051830795e1d8eeabfffc7b3606d0c36b040
vmhtomosia <hung.vu@tomosia.com>
2024-09-09 14:20:01 +0700

    change url

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
   const initializeLiff = async () => {
     try {
       await liff.init({
-        liffId: '2006286965-zwb6bj19',
+        liffId: '2003228422-o4GZzLYl',//  2006286965-zwb6bj19
         withLoginOnExternalBrowser: true,
       });
       setLiffObject(liff);
